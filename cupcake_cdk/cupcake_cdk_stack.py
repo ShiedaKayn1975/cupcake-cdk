c4a1861a7e226437d007ecb8768881dd51dcadf4
littleBoy1945 <nguyenta.hebestorm@gmail.com>
2023-02-18 11:38:37 +0700

    init s3 and cloudfront

--- cupcake_cdk/cupcake_cdk_stack.py
+++ cupcake_cdk/cupcake_cdk_stack.py
@@ -1,11 +1,10 @@
 from constructs import Construct
 from aws_cdk import (
-    Duration,
+    aws_cloudfront_origins,
     Stack,
     aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
+    aws_s3 as s3,
+    aws_cloudfront as cf
 )
 
 
@@ -14,13 +13,26 @@ class CupcakeCdkStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "CupcakeCdkQueue",
-            visibility_timeout=Duration.seconds(300),
+        bucket = s3.Bucket(
+            self, 
+            "cupcake_cms",
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL
         )
-
-        topic = sns.Topic(
-            self, "CupcakeCdkTopic"
+        
+        oai = cf.OriginAccessIdentity(self, "OAI", comment="Connects CF with S3")
+        bucket.grant_read(oai)
+        
+        distribution = cf.Distribution(
+            self,
+            "CDN",
+            minimum_protocol_version=cf.SecurityPolicyProtocol.TLS_V1_2_2018,
+            default_behavior=cf.BehaviorOptions(
+                allowed_methods=cf.AllowedMethods.ALLOW_ALL,
+                origin=aws_cloudfront_origins.S3Origin(
+                    bucket=bucket,
+                    origin_access_identity=oai,
+                    origin_path="/",
+                ),
+                viewer_protocol_policy=cf.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+            )
         )
-
-        topic.add_subscription(subs.SqsSubscription(queue))
